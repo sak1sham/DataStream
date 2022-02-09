a0dffbb5ad92f037db3b476ef7c93fd878421ec1
Saksham Garg <saksham2700@gmail.com>
2022-02-09 18:25:30 +0530

    minor edits

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             for curr_table in db['tables']:
                 year, month, day, week, day_of_week, hour, minute, second = evaluate_cron(curr_table['cron'])
                 curr_table['table_unique_id'] = db['source']['source_type'] + ":" + db['source']['db_name'] + ":" + curr_table['table_name']
-                if(curr_table['table_unique_id'] in custom_records_to_run):
+                if(len(custom_records_to_run) == 0 or curr_table['table_unique_id'] in custom_records_to_run):
                     scheduler.add_job(process_sql_table, 'cron', args=[db, curr_table], id=curr_table['table_unique_id'], year=year, month=month, day=day, week=week, day_of_week=day_of_week, hour=hour, minute=minute, second=second, timezone=pytz.timezone('Asia/Calcutta'))
         if(db['source']['source_type'] == 'mongo'):
             if('collections' not in db.keys()):
@@ -50,7 +50,7 @@ if __name__ == "__main__":
             for curr_collection in db['collections']:
                 year, month, day, week, day_of_week, hour, minute, second = evaluate_cron(curr_collection['cron'])
                 curr_collection['collection_unique_id'] = db['source']['source_type'] + ":" + db['source']['db_name'] + ":" + curr_collection['collection_name']
-                if(curr_collection['collection_unique_id'] in custom_records_to_run):
+                if(len(custom_records_to_run) == 0 or curr_collection['collection_unique_id'] in custom_records_to_run):
                     scheduler.add_job(process_mongo_collection, 'cron', args=[db, curr_collection], id=curr_collection['collection_unique_id'], year=year, month=month, day=day, week=week, day_of_week=day_of_week, hour=hour, minute=minute, second=second, timezone=pytz.timezone('Asia/Calcutta'))
     logging.info('Added job(s) to the scheduler.')
     uvicorn.run(app, port=int(os.getenv('PORT')), host=os.getenv("HOST"))
