8c469201e6aae8515f621ca2c1c99e8c8fa4636a
Saksham Garg <saksham2700@gmail.com>
2022-02-11 12:36:37 +0530

    Minor edits

--- config/migration_mapping.py
+++ config/migration_mapping.py
@@ -209,7 +209,7 @@ mapping = [
                 },
                 'bookmark': 'updatedAt',
                 'archive': False,
-                'cron': '* * * * * 12 28 0',
+                'cron': '* * * * * 12 38 0',
                 'to_partition': True,
             },
             {
