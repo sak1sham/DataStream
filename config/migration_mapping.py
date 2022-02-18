824cf5c08208e0d74c028d96079afaa22d06e6e5
Saksham Garg <saksham2700@gmail.com>
2022-02-18 11:09:22 +0530

    minor edits

--- config/migration_mapping.py
+++ config/migration_mapping.py
@@ -113,7 +113,7 @@ mapping = [
                 'fields': {},
                 'bookmark': False,
                 'archive': False,
-                'cron': '* * * * * * 34 0',
+                'cron': '* * * * * * */1 0',
                 'to_partition': True,
                 'is_dump': True,
                 'partition_col': 'migration_snapshot_date'
--- dst/redshift.py
+++ dst/redshift.py
@@ -19,6 +19,7 @@ class RedshiftSaver:
             user = db_destination['user'],
             password = db_destination['password']
         )
+        print(self.conn)
         self.schema = db_destination['schema']
 
     def inform(self, message: str = "") -> None:
@@ -27,27 +28,29 @@ class RedshiftSaver:
     def warn(self, message: str = "") -> None:
         logging.warning(self.unique_id + ": " + message)
 
-    def save(self, processed_data: Dict[str, Any] = {}, c_partition: str = "") -> None:
+    def save(self, processed_data: Dict[str, Any] = {}, c_partition: List[str] = []) -> None:
         # There is no need of c_partition here, it's just taken to be consistent with S3Saver.save()
         self.name_ = processed_data['name']
         file_name = self.s3_location + self.name_ + "/"
+        print(processed_data['df_insert'])
         self.inform("Attempting to insert " + str(processed_data['df_insert'].memory_usage(index=True).sum()) + " bytes.")
         if(processed_data['df_insert'].shape[0] > 0):
             wr.redshift.copy(
                 df = processed_data['df_insert'],
-                path = file_name,
                 con = self.conn,
-                schema = self.schema,
-                table = self.name_,
+                path = "s3://data-migration-server/redshift/",
+                schema = 'migration_service',
+                table = 'my_table2',
                 mode = "append",
             )
         self.inform("Inserted " + str(processed_data['df_insert'].shape[0]) + " records.")
-
+        print(wr.redshift.read_sql_table(table="my_table2", schema="migration_service", con=self.conn), "\n")
+        print(wr.redshift.read_sql_table(table="my_table", schema="migration_service", con=self.conn), "\n")
         self.inform("Attempting to update " + str(processed_data['df_update'].memory_usage(index=True).sum()) + " bytes.")    
         if(processed_data['df_update'].shape[0] > 0):
             wr.redshift.copy(
                 df = processed_data['df_update'],
-                path=file_name,
+                path = file_name,
                 con = self.conn,
                 schema = self.schema,
                 table = self.name_,
