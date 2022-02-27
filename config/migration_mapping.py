92bf6287287b37ca38e1e08b04d2cf51e6cfef9f
Saksham Garg <saksham2700@gmail.com>
2022-02-27 20:15:57 +0530

    test edits

--- config/migration_mapping.py
+++ config/migration_mapping.py
@@ -95,10 +95,10 @@ mapping = {
     "cmdb_tables_to_s3": {
         'source': {
             'source_type': 'sql',
-            'url': 'cmdb-rr2.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+            'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
             'db_name': 'cmdb',
-            'username': 'cm',
-            'password': 'cm'
+            'username': 'saksham_garg',
+            'password': '3y5HMs^2qy%&Kma'
         },
         'destination': {
             'destination_type': 's3',
@@ -107,7 +107,7 @@ mapping = {
         'tables': [
             {
                 'table_name': 'inventory_snapshot_wms',
-                'cron': '* * * * * */1 0 0',
+                'cron': 'self-managed',
                 'to_partition': True,
                 'partition_col': 'migration_snapshot_date',
                 'partition_col_format': 'datetime',
