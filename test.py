ca1296d46934c0a2b9b16ff7ee8124bb1e877192
Manish Prakash <manish.prakash@citymall.live>
2022-03-13 15:00:30 +0530

    test

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ import pytz
 from fastapi import FastAPI
 import uvicorn
 
-# client = MongoClient('mongodb+srv://saksham:xwNTtWtOnTD2wYMM@supportservicev2.3md7h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-# database_ = client['support-service']
-# coll = database_['support_tickets']
+client = MongoClient('mongodb+srv://saksham:xwNTtWtOnTD2wYMM@supportservicev2.3md7h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+database_ = client['support-service']
+coll = database_['support_tickets']
 
-# start = pytz.utc.localize(datetime.datetime(2022, 3, 4))
-# end = pytz.utc.localize(datetime.datetime(2022, 3, 5))
+start = pytz.utc.localize(datetime.datetime(2022, 3, 4))
+end = pytz.utc.localize(datetime.datetime(2022, 3, 5))
 
-# curs = coll.find({'created_at': {'$gte': start, '$lt': end}})
-# ls = list(curs)
-# print(len(ls))
+curs = coll.find({'created_at': {'$gte': start, '$lt': end}})
+ls = list(curs)
+print(len(ls))
 
-print(1)
+# print(1)
 # while True:
 #     print(1)
 #     sleep(1000)
