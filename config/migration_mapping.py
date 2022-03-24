64fe10cf1ef134f69bbb7f4d0ab33570d65a6fcc
Saksham Garg <saksham2700@gmail.com>
2022-03-24 19:40:37 +0530

    reverted bucket-name

--- config/migration_mapping.py
+++ config/migration_mapping.py
@@ -11,7 +11,7 @@ mapping = {
         },
         'destination': {
             'destination_type': 's3',
-            's3_bucket_name': 'data-migration-server',
+            's3_bucket_name': 'database-migration-service-prod',
         },
         'collections': [
             {
