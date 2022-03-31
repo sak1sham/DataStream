5c238f4142fab78d49b2434f7a53412fa50668cc
Anchit Tandon <anchit.tandon@gmail.com>
2022-03-31 18:42:24 +0530

    migrations for all tables in sheet from line tbl_user to orders

---
+++ config/jobs/spoke_cash_transition_ledger.py
@@ -0,0 +1,27 @@
+mapping = {
+    'source': {
+        'source_type': 'sql',
+        'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+        'db_name': 'cmdb',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'destination': {
+        'destination_type': 's3',
+        's3_bucket_name': 'database-migration-service-prod'
+    },
+    'tables': [            
+        {
+            'table_name': 'spoke_cash_transition_ledger',
+            'cron': '* * * * * 22 10 0',
+            'mode': 'syncing',
+            'primary_key': 'id',
+            'primary_key_datatype': 'int',
+            'to_partition': True,
+            'partition_col': 'created_at',
+            'partition_col_format': 'datetime',
+            'bookmark': 'updated_at',
+            'improper_bookmarks': False
+        },
+    ]
+}
---
+++ config/jobs/tbl_admin.py
@@ -0,0 +1,27 @@
+mapping = {
+    'source': {
+        'source_type': 'sql',
+        'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+        'db_name': 'cmdb',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'destination': {
+        'destination_type': 's3',
+        's3_bucket_name': 'database-migration-service-prod'
+    },
+    'tables': [
+        {
+            'table_name': 'tbl_admin',
+            'cron': '* * * * * 22 10 0',
+            'mode': 'syncing',
+            'primary_key': 'admin_id',
+            'primary_key_datatype': 'int',
+            'to_partition': True,
+            'partition_col': 'admin_created',
+            'partition_col_format': 'datetime',
+            'bookmark': 'admin_updated',
+            'improper_bookmarks': False
+        },
+    ]
+}
