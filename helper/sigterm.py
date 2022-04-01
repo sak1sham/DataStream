f6fedb0c5beb6f95b8afda058fa74506aa2276a5
Saksham Garg <saksham2700@gmail.com>
2022-04-01 19:39:39 +0530

    proper indentation

--- helper/sigterm.py
+++ helper/sigterm.py
@@ -2,20 +2,20 @@ import signal
 import time
 
 class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self, *args):
-    self.kill_now = True
+    def exit_gracefully(self, *args):
+        self.kill_now = True
 
 if __name__ == '__main__':
-  killer = GracefulKiller()
-  while not killer.kill_now:
-    print("hi")
-    time.sleep(10)
-    print("doing something in a loop ...")
-    break
-   
-  print("End of the program. I was killed gracefully :)")
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        print("hi")
+        time.sleep(10)
+        print("doing something in a loop ...")
+        break
+
+    print("End of the program. I was killed gracefully :)")
