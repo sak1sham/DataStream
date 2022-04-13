bb7ba4b9cb416875fe28c86235332bd8949aa247
Anchit Tandon <anchit.tandon@gmail.com>
2022-04-13 15:45:27 +0530

    Changed cron for jobs which had failed to check right now and test edits for inventory_transactions

--- config/jobs/inventory_transactions.py
+++ config/jobs/inventory_transactions.py
@@ -2,7 +2,7 @@ mapping = {
     'source': {
         'source_type': 'sql',
         'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
-        'db_name': 'cmdb',
+        'db_name': 'wmsdb',
         'username': 'saksham_garg',
         'password': '3y5HMs^2qy%&Kma'
     },
@@ -10,16 +10,18 @@ mapping = {
         'destination_type': 's3',
         's3_bucket_name': 'database-migration-service-prod'
     },
-    'tables': [            
+    'tables': [
         {
-            'table_name': 'idfas_to_ban_usage',
+            'table_name': 'inventory_transaction_view',
             'cron': 'self-managed',
-            'mode': 'logging',
-            'primary_key': 'id',
-            'primary_key_datatype': 'int',
+            'mode': 'syncing',
+            'primary_key': 'transaction_id',
+            'primary_key_datatype': 'str',
             'to_partition': True,
             'partition_col': 'created_at',
             'partition_col_format': 'datetime',
+            # 'bookmark': 'updated_at',
+            # 'improper_bookmarks': False,
             'batch_size': 10000,
         },
     ]
