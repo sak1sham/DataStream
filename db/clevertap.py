99c679ede98c4c07522cb2c4f36cfd532e8fd6e5
Mukesh Bhati <mukeshbhatisikar@gmail.com>
2022-04-14 10:32:47 +0530

    fixes

--- db/clevertap.py
+++ db/clevertap.py
@@ -26,7 +26,7 @@ class EventsAPIManager:
 
     def get_event_cursor(self, event_name: str, from_date: str, to_date: str):
         params = {
-            "batch_size": 5000, 
+            "batch_size": 10000, 
             "events": "false"
         }
         payload = {
