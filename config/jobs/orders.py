6b2796a36a93243138b81a639bbbc906ee0dc260
Saksham Garg <saksham2700@gmail.com>
2022-04-19 15:10:28 +0530

    test edits

--- config/jobs/orders.py
+++ config/jobs/orders.py
@@ -6,13 +6,9 @@ mapping = {
         'username': 'saksham_garg',
         'password': '3y5HMs^2qy%&Kma'
     },
-    "destination": {
-        'destination_type': 'redshift',
-        'host': 'cm-redshift-1.cyl4ilkelm5m.ap-south-1.redshift.amazonaws.com',
-        'database': 'cmwh',
-        'user': 'cmadmin',
-        'password': 'kgDzH6Zy5xZ6HHx',
-        's3_bucket_name': 'data-migration-service-dev',
+    'destination': {
+        'destination_type': 's3',
+        's3_bucket_name': 'database-migration-service-prod'
     },
     'tables': [            
         {
@@ -21,32 +17,12 @@ mapping = {
             'mode': 'syncing',
             'primary_key': 'order_id',
             'primary_key_datatype': 'int',
-            'to_partition': False,
+            'to_partition': True,
             'partition_col': 'created_at',
             'partition_col_format': 'datetime',
             'bookmark': 'updated_at',
             'improper_bookmarks': False,
             'batch_size': 10000,
-            'lob_fields_length': {
-                'address_name': 5000,
-                'address_address1': 5000,
-                'address_address2': 5000,
-                'address_landmark': 5000,
-                'tracking_info': 30000,
-                'extra_info': 30000,
-                'order_cancellation_error': 5000,
-                'order_error': 5000,
-                'order_error_details': 5000,
-                'order_cancellation_reason': 5000,
-                'tags': 45000,
-                'touchpoint_formatted_address': 5000,
-                'cx_formatted_address': 5000,
-                'order_rescheduled_reason': 5000,
-                'review_cx': 5000,
-                'order_catalogue_name': 5000,
-                'order_warehouse_name': 5000,
-                'locality': 5000,
-            }
         },
     ]
 }
