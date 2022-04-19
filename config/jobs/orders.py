a37d77a3c5bcdb9d518e251cd1e1b23ee06fae4b
Saksham Garg <saksham2700@gmail.com>
2022-04-19 12:24:47 +0530

    Test edits

--- config/jobs/orders.py
+++ config/jobs/orders.py
@@ -38,7 +38,7 @@ mapping = {
                 'order_error': 5000,
                 'order_error_details': 5000,
                 'order_cancellation_reason': 2000,
-                'tags': 5000,
+                'tags': 10000,
                 'touchpoint_formatted_address': 1000,
                 'cx_formatted_address': 1000,
                 'order_rescheduled_reason': 1000,
