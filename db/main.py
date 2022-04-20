dd4da9a659df632a31e103998ae43695ff17750e
Saksham Garg <saksham2700@gmail.com>
2022-04-20 18:00:44 +0530

    changed notification pattern

--- db/main.py
+++ db/main.py
@@ -8,6 +8,8 @@ from typing import Dict, Any
 from helper.exceptions import IncorrectMapping
 from helper.logger import logger
 import traceback
+import time
+import datetime
 from notifications.slack_notify import send_message
 from config.settings import settings
 
@@ -40,16 +42,19 @@ class DMS_importer:
     
     def process(self):
         try:
+            start = time.time()
             result = self.obj.process()
+            end = time.time()
+            time_taken = str(datetime.timedelta(seconds=int(end-start)))
             if('notify' in settings.keys() and settings['notify']):
                 msg = "Migration completed for *"
                 msg += str(self.name)
                 msg += "* from database "
-                msg += self.db['source']['source_type'] + " : *" + self.db['source']['db_name']
-                msg += "*. "
+                msg += "*" + self.db['source']['db_name'] + "* (" + self.db['source']['source_type'] + ") to *" + self.db['destination']['destination_type'] + "*:\n"
+                msg += "Total time taken: " + time_taken + "\n"
                 if(isinstance(result, tuple)):
-                    msg += "Inserted " + str(result[0]) + " records and updated " + str(result[1]) + " records."
-                msg += " :tada:"
+                    msg += "Insertions: " + str(result[0]) + "\n"
+                    msg += "Updations: " + str(result[1])
                 try:
                     slack_token = settings['slack_notif']['slack_token']
                     channel = self.curr_mapping['slack_channel'] if 'slack_channel' in self.curr_mapping and self.curr_mapping['slack_channel'] else settings['slack_notif']['channel']
