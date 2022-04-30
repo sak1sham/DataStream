71c0aceaedf6a37baa918d4925a440dda116fb31
Saksham Garg <saksham2700@gmail.com>
2022-04-30 21:39:30 +0530

    Properly format number of objects deleted from s3

--- src/suppliments/empty_s3_bucket.py
+++ src/suppliments/empty_s3_bucket.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
             for obj in objects_for_deletion:
                 wr.s3.delete_objects(path=obj)
                 count += 1
-            logger.inform(s = "Deleted {0} objects from bucket {1}.".format(count, args[0]))
+            logger.inform(s = "Deleted {0} objects from bucket {1}.".format("{:,}".format(count), args[0]))
             if('notify' in settings.keys() and settings['notify']):
-                send_message(msg = "Deleted {0} objects from S3 bucket {1}.".format(count, args[0]), channel=settings['slack_notif']['channel'], slack_token=settings['slack_notif']['slack_token'])
+                send_message(msg = "Deleted {0} objects from S3 bucket {1}.".format("{:,}".format(count), args[0]), channel=settings['slack_notif']['channel'], slack_token=settings['slack_notif']['slack_token'])
                 logger.inform(s = "Notification sent.")
         except Exception as e:
             logger.err(traceback.format_exc())
