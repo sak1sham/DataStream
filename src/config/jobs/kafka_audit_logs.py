b5bf737fba6fe9c8a4369893211b589edc4ac154
Anchit Tandon <anchit.tandon@gmail.com>
2022-05-04 15:10:52 +0530

    updated bucket name in file

--- src/config/jobs/kafka_audit_logs.py
+++ src/config/jobs/kafka_audit_logs.py
@@ -12,7 +12,7 @@ mapping = {
     },
     'destination': {
         'destination_type': 's3',
-        's3_bucket_name': 'data-migration-server-prod',
+        's3_bucket_name': 'database-migration-service-prod',
     },
     'topics': [
         {
