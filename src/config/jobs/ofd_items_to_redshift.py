283a40369b08cdcbd88eb2f33bcb60fe60a41d5b
Saksham Garg <saksham2700@gmail.com>
2022-05-05 19:14:14 +0530

    Added other files

--- src/config/jobs/ofd_items_to_redshift.py
+++ src/config/jobs/ofd_items_to_redshift.py
@@ -16,17 +16,27 @@ mapping = {
     },
     'tables': [
         {
-            'table_name': 'notifications',
+            'table_name': 'ofd_items',
             'cron': 'self-managed',
             'mode': 'syncing',
             'primary_key': 'id',
             'primary_key_datatype': 'int',
-            'to_partition': True,
-            'partition_col': 'created_at',
-            'partition_col_format': 'datetime',
-            'bookmark': 'sent_at',
+            'bookmark': 'updated_at_for_pipeline',
             'improper_bookmarks': False,
             'batch_size': 10000,
+            'lob_fields_length': {
+                'shipments': 65535,
+                'order_item_status': 3036,
+                'route_name': 3036,
+                'spoke_name': 3036,
+                'delivery_boy_username': 3036,
+            },
+            'grace_updation_lag': {
+                'days': 1
+            },
+            'buffer_updation_lag':{
+                'hours': 2,
+            }
         },
     ]
 }
