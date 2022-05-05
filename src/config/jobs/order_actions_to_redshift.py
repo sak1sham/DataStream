416a2b074cc9c04e5531975a6144d31c7da2bcc5
Anchit Tandon <anchit.tandon@gmail.com>
2022-05-05 19:11:38 +0530

    Added job mapping for order-actions-to-redshift

---
+++ src/config/jobs/order_actions_to_redshift.py
@@ -0,0 +1,40 @@
+mapping = {
+    'source': {
+        'source_type': 'sql',
+        'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+        'db_name': 'cmdb',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'destination': {
+        'destination_type': 'redshift',
+        'host': 'cm-redshift-1.cyl4ilkelm5m.ap-south-1.redshift.amazonaws.com',
+        'database': 'cmwh',
+        'user': 'cmadmin',
+        'password': 'kgDzH6Zy5xZ6HHx',
+        's3_bucket_name': 'database-migration-service-prod',
+    },
+    'tables': [            
+        {
+            'table_name': 'order_actions',
+            'cron': 'self-managed',
+            'mode': 'syncing',
+            'primary_key': 'id',
+            'primary_key_datatype': 'int',
+            'bookmark': 'updated_at',
+            'improper_bookmarks': False,
+            'batch_size': 10000,
+            'lob_fields_length': {
+                'action_name': 3036,
+                'old_value': 3036,
+                'new_value': 3036,
+            },
+            'grace_updation_lag': {
+                'days': 1
+            },
+            'buffer_updation_lag':{
+                'hours': 2,
+            }
+        },
+    ]
+}
