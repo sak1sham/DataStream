580a80c34098d5fcd5906240153d78d951f658db
Saksham Garg <saksham2700@gmail.com>
2022-05-10 08:15:22 +0530

    test

--- src/config/jobs/mongo_s3_support.py
+++ src/config/jobs/mongo_s3_support.py
@@ -1,7 +1,7 @@
 mapping = {
     'source': {
         'source_type': 'mongo',
-        'url': 'mongodb://manish:ACVVCH7t7rqd8kB8@supportv2.cbo3ijdmzhje.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false',
+        'url': 'mongodb://manish:ACVVCH7t7rqd8kB8@supportv2.cluster-cbo3ijdmzhje.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false',
         'db_name': 'support-service',
         'certificate_file': 'rds-combined-ca-bundle.pem'
     },
