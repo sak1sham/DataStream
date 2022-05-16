75dc8f4dec7ab44c07a44251b963cda035fb3493
Anchit Tandon <anchit.tandon@gmail.com>
2022-05-16 10:40:24 +0530

    updated routes mapping from dumping to syncing

--- src/config/jobs/routes.py
+++ src/config/jobs/routes.py
@@ -1,5 +1,3 @@
-query_1 = """select * from routes where completed_at <= '2021-05-10'::timestamp and completed_at is not null"""
-
 mapping = { 
     'source': { 
         'source_type': 'sql', 
@@ -12,49 +10,25 @@ mapping = {
         'destination_type': 's3', 
         's3_bucket_name': 'database-migration-service-prod'
     }, 
-    # 'tables': [ 
-    #     {
-    #         'table_name': 'routes', 
-    #         'cron': 'self-managed', 
-    #         'mode': 'syncing',
-    #         'primary_key': 'id',
-    #         'primary_key_datatype': 'int',
-    #         'to_partition': True, 
-    #         'partition_col': 'started_at',
-    #         'partition_col_format': 'datetime',
-    #         'bookmark': 'updated_at_for_pipeline', 
-    #         'improper_bookmarks': False, 
-    #         'batch_size': 10000, 
-    #         'buffer_updation_lag': {
-    #             'hours': 2,
-    #         } ,
-    #         'grace_updation_lag': {
-    #             'days': 1
-    #         },
-    #     },
-    # ]
-    'tables': [
+    'tables': [ 
         {
-            'table_name': 'routes',
-            'cron': 'self-managed',
-            'to_partition': True,
+            'table_name': 'routes', 
+            'cron': 'self-managed', 
+            'mode': 'syncing',
+            'primary_key': 'id',
+            'primary_key_datatype': 'int',
+            'to_partition': True, 
             'partition_col': 'started_at',
             'partition_col_format': 'datetime',
-            'mode': 'dumping',
-            'fetch_data_query': query_1,
-            'fields': {
-                'id': 'int',
-                'priority': 'int',
-                'total_cash_collected': 'int',
-                'total_upi_collected': 'int',
-                'order_id': 'int',
-                'delivery_date': 'datetime',
-                'reached_at': 'datetime',
-                'started_at': 'datetime',
-                'completed_at': 'datetime',
-                'missed_at': 'datetime',          
+            'bookmark': 'updated_at_for_pipeline', 
+            'improper_bookmarks': False, 
+            'batch_size': 10000, 
+            'buffer_updation_lag': {
+                'hours': 2,
+            } ,
+            'grace_updation_lag': {
+                'days': 1
             },
-            'batch_size': 10000,
         },
     ]
 }
