8eeb6ffe48d76a9e8d7eafecfafde9666d28b0d8
Saksham Garg <saksham2700@gmail.com>
2022-05-17 12:38:32 +0530

    Added mappings

---
+++ src/config/jobs/combo_skus_to_analytics_pgsql.py
@@ -0,0 +1,34 @@
+mapping = {
+    'source': {
+        'source_type': 'sql',
+        'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+        'db_name': 'cmdb',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'destination': {
+        'destination_type': 'pgsql',
+        'url': '3.108.43.163',
+        'db_name': 'dms',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'tables': [            
+        {
+            'table_name': 'combo_skus',
+            'cron': 'self-managed',
+            'mode': 'syncing',
+            'primary_key': 'id',
+            'primary_key_datatype': 'int',
+            'bookmark': 'updated_at_for_pipeline',
+            'improper_bookmarks': False,
+            'batch_size': 10000,
+            'buffer_updation_lag':{
+                'hours': 2,
+            },
+            'grace_updation_lag': {
+                'days': 1
+            },
+        },
+    ]
+}
