52acbb7717ff27d95888ac4f1b6f5ca6dd5646cf
Saksham Garg <saksham2700@gmail.com>
2022-05-17 12:42:26 +0530

    Changed grace updation lag

--- src/config/jobs/price_drop_items_to_analytics_pgsql.py
+++ src/config/jobs/price_drop_items_to_analytics_pgsql.py
@@ -27,7 +27,7 @@ mapping = {
                 'hours': 2,
             } ,
             'grace_updation_lag': {
-                'days': 1
+                'hours': 4
             },
         },
     ]
