b51d956fd4755acd74613db6c25cec14a4579493
Saksham Garg <saksham2700@gmail.com>
2022-06-03 11:00:50 +0530

    Modifications

--- src/testing/migration_mapping.py
+++ src/testing/migration_mapping.py
@@ -2,8 +2,8 @@ from typing import Dict
 import importlib.util
 
 def get_mapping(id: str) -> Dict:
-    f_name = "config/jobs/" + id + ".py"
-    mod_name = id + "./py"
+    f_name = f"config/jobs/{id}.py"
+    mod_name = f"{id}./py"
     spec = importlib.util.spec_from_file_location(mod_name, f_name)
     foo = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(foo)
