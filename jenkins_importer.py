91100074720c60b7e2042b97f0da411f2158d4be
Saksham Garg <saksham2700@gmail.com>
2022-06-06 19:01:32 +0530

    Added jenkins importer

---
+++ jenkins_importer.py
@@ -0,0 +1,51 @@
+'''with open(f"deployment/jenkins/production/commands/{file_name_pg[:-3].replace('_', '-')}-values.yaml", 'w') as f:
+    text = ''
+    with open('deployment/jenkins/production/commands/analytics-cl-funnel-to-analytics-pgsql-values.yaml', 'r') as file:
+        text = file.read()
+    text = text.replace('analytics-cl-funnel-to-analytics-pgsql', file_name_pg[:-3].replace('_', '-'))
+    f.write(text)'''
+
+import os
+github_actions = os.listdir('.github/workflows/')
+jenkins_files = os.listdir('deployment/jenkins/production/commands/')
+
+g_map = {}
+for l in github_actions:
+    g_map[l.replace('prod.kube.', '').replace('.yaml', '')] = l
+
+j_map = {}
+for l in jenkins_files:
+    g_map[l.replace('-values.yaml', '')] = l
+
+not_present = g_map.keys() - j_map.keys()
+
+print(not_present)
+
+for uid in not_present:
+    text = ""
+    with open(f".github/workflows/prod.kube.{uid}.yaml", 'r') as f:
+        text = f.read()
+
+    new_text = ""
+    with open("deployment/jenkins/production/commands/adminroles-values.yaml", 'r') as f:
+        new_text = f.read()
+    
+    new_text.replace('adminroles', uid)
+    
+    cron = "30 16 * * *"
+    if 'redshift' in uid:
+        cron = "30 18 * * *"
+    if 'pgsql' in uid:
+        cron = "30 20 * * *"
+
+    cron_str = f'{uid}: "{cron}"'
+    new_text = new_text.replace('adminroles: "0 16 * * *"', cron_str)
+
+    if 'deploymentEnabled: false' in text:
+        new_text = new_text.replace('deploymentEnabled: true', 'deploymentEnabled: false').replace('jobsEnabled: false', 'jobsEnabled: true')
+    else:
+        new_text = new_text.replace('deploymentEnabled: false', 'deploymentEnabled: true').replace('jobsEnabled: true', 'jobsEnabled: false')
+
+    
+    with open(f"deployment/jenkins/production/commands/{uid}-values.yaml", 'w') as f:
+        f.write(new_text)
