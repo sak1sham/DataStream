c534a773ca5d68631eb5a2c00224d2ca4cff23df
Saksham Garg <saksham2700@gmail.com>
2022-06-06 17:57:14 +0530

    cooler than before

--- mapping_generator.py
+++ mapping_generator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import numpy as np
+import re
 
 df = pd.read_csv('abc.csv')
 df = df.replace({np.nan:None})
@@ -123,3 +124,31 @@ for index, row in df.iterrows():
         text = text.replace('analytics-cl-funnel-to-analytics-pgsql', file_name_pg[:-3].replace('_', '-'))
         f.write(text)
         
+    with open(f"deployment/jenkins/production/commands/{file_name[:-3].replace('_', '-')}-values.yaml", 'w') as f:
+        text = ''
+        with open('deployment/jenkins/production/commands/analytics-cl-funnel-values.yaml', 'r') as file:
+            text = file.read()
+        text = text.replace('analytics-cl-funnel', file_name[:-3].replace('_', '-'))
+        f.write(text)
+
+    with open(f"deployment/jenkins/production/commands/{file_name_pg[:-3].replace('_', '-')}-values.yaml", 'w') as f:
+        text = ''
+        with open('deployment/jenkins/production/commands/analytics-cl-funnel-to-analytics-pgsql-values.yaml', 'r') as file:
+            text = file.read()
+        text = text.replace('analytics-cl-funnel-to-analytics-pgsql', file_name_pg[:-3].replace('_', '-'))
+        f.write(text)
+
+    text = ''
+    new_text = ''
+    with open('deployment/jenkins/production/jenkinsfiles/serviceDeployment', 'r') as file:
+        text = file.read()
+        pat = r"value:'[a-zA-Z,-]*'"
+        m = re.search(pat, text)
+        vals = m.group(0)
+        jobs = vals.split(':')[1][1:-1]
+        new_jobs = f"{jobs},{file_name[:-3].replace('_', '-')},{file_name_pg[:-3].replace('_', '-')}"
+        new_vals = f"values:'{new_jobs}'"
+        new_text = re.sub(pat, new_vals, text)
+
+    with open('deployment/jenkins/production/jenkinsfiles/serviceDeployment', 'w') as f:
+        f.write(new_text)
