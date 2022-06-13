e8da6661a4d78d40bb19cf9ac5e0f6d68a1cda81
Saksham Garg <saksham2700@gmail.com>
2022-06-13 12:29:36 +0530

    edits

--- src/config/jobs/admin_sessions_to_analytics_pgsql.py
+++ src/config/jobs/admin_sessions_to_analytics_pgsql.py
@@ -1,19 +1,16 @@
 mapping = {
     "destination": {
-        "destination_type": "pgsql",
-        "specifications": {
-            'ec2_1': {
-                "db_name": "dms",
-                "password": "3y5HMs^2qy%&Kma",
-                "url": "3.108.43.163",
-                "username": "saksham_garg"
-            },
-            'ec2_2': {
-                "db_name": "dms",
-                "password": "3y5HMs^2qy%&Kma",
-                "url": "13.233.225.181",
-                "username": "saksham_garg"
-            }
+        'ec2_1': {
+            "db_name": "dms",
+            "password": "3y5HMs^2qy%&Kma",
+            "url": "3.108.43.163",
+            "username": "saksham_garg"
+        },
+        'ec2_2': {
+            "db_name": "dms",
+            "password": "3y5HMs^2qy%&Kma",
+            "url": "13.233.225.181",
+            "username": "saksham_garg"
         }
     },
     "source": {
