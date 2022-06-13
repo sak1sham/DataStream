1c4c47ff5db4bb9ce8dcd918aa90a5e92dd32729
Anchit Tandon <anchit.tandon@gmail.com>
2022-06-13 18:01:08 +0530

    Added job for scratch-cards to s3

---
+++ src/config/jobs/scratch_cards.py
@@ -0,0 +1,37 @@
+mapping = {
+    "destination": {
+        "destination_type": "s3",
+        "specifications": [
+            {
+                "s3_bucket_name": "database-migration-service-prod"
+            }
+        ]
+    },
+    "source": {
+        "db_name": "cmdb",
+        "password": "3y5HMs^2qy%&Kma",
+        "source_type": "pgsql",
+        "url": "cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com",
+        "username": "saksham_garg"
+    },
+    "tables": [
+        {
+            "batch_size": 10000,
+            "bookmark": "updated_at_for_pipeline",
+            "buffer_updation_lag": {
+                "hours": 2
+            },
+            "cron": "self-managed",
+            "grace_updation_lag": {
+                "days": 1
+            },
+            "improper_bookmarks": False,
+            "mode": "syncing",
+            "partition_col": "created_at",
+            "partition_col_format": "datetime",
+            "primary_key": "id",
+            "primary_key_datatype": "int",
+            "table_name": "scratch_cards",
+        }
+    ]
+}
