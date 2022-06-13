5d758b529bdf5246bdad2e85ee9840aa6d4d96a7
Mohit Bahri <mohitbahri.2593@gmail.com>
2022-06-13 14:35:45 +0530

    mapping updation

---
+++ src/config/jobs/product_categories_dump.py
@@ -0,0 +1,32 @@
+mapping = {
+    'source': {
+        'source_type': 'pgsql',
+        'url': 'cmdb-rr.cbo3ijdmzhje.ap-south-1.rds.amazonaws.com',
+        'db_name': 'cmdb',
+        'username': 'saksham_garg',
+        'password': '3y5HMs^2qy%&Kma'
+    },
+    'destination': { 
+        'destination_type': 's3', 
+        'specifications': [
+            {
+                's3_bucket_name': 'database-migration-service-prod' 
+            }
+        ]
+    }, 
+    'tables': [
+        {
+            'table_name': 'product_categories',
+            'mode': 'dumping',
+            'partition_col': 'migration_snapshot_date',
+            'partition_col_format': 'datetime',
+            'batch_size': 10000,
+            'buffer_updation_lag':{
+                'hours': 2,
+            },
+            'grace_updation_lag': {
+                'days': 1
+            },
+        },
+    ]
+}
