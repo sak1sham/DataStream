19f280b58ebedd3e563c4357c641101473516ed0
Mohit Bahri <mohitbahri.2593@gmail.com>
2022-06-24 18:15:27 +0530

    Add ec2_3

--- src/config/jobs/wms_public_rm_inventory_transactions_pgsql.py
+++ src/config/jobs/wms_public_rm_inventory_transactions_pgsql.py
@@ -12,20 +12,28 @@ mapping = {
     }, 
     "destination": {
         'ec2_1': {
-            "db_name": "wmsdb",
-            "schema": "public",
+            "db_name": "cmdb",
+            "schema": "wms",
             "password": os.getenv('DB_PASSWORD'),
             "url": "dms2.citymall.dev",
             "username": os.getenv('DB_USERNAME'),
             "destination_type": "pgsql",
         },
         'ec2_2': {
-            "db_name": "wmsdb",
-            "schema": "public",
+            "db_name": "cmdb",
+            "schema": "wms",
             "password": os.getenv('DB_PASSWORD'),
             "url": "dms1.citymall.dev",
             "username": os.getenv('DB_USERNAME'),
             "destination_type": "pgsql",
+        },
+        'ec2_3': {
+            "db_name": "cmdb",
+            "schema": "wms",
+            "password": os.getenv('DB_PASSWORD'),
+            "url": "dms3.citymall.dev",
+            "username": os.getenv('DB_USERNAME'),
+            "destination_type": "pgsql"            
         }
     },
     'tables': [ 
