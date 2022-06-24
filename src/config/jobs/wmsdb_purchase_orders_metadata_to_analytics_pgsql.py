1c930fe29e3c7521f78de3ed826c1c464d2bbab5
Mohit Bahri <mohitbahri.2593@gmail.com>
2022-06-24 18:22:35 +0530

    comment ec2_1 and ec2_3

--- src/config/jobs/wmsdb_purchase_orders_metadata_to_analytics_pgsql.py
+++ src/config/jobs/wmsdb_purchase_orders_metadata_to_analytics_pgsql.py
@@ -4,14 +4,14 @@ load_dotenv()
 
 mapping = {
     "destination": {
-        'ec2_1': {
-            "db_name": "cmdb",
-            "schema": "wms",
-            "password": os.getenv('DB_PASSWORD'),
-            "url": "dms2.citymall.dev",
-            "username": os.getenv('DB_USERNAME'),
-            "destination_type": "pgsql",
-        },
+        # 'ec2_1': {
+        #     "db_name": "cmdb",
+        #     "schema": "wms",
+        #     "password": os.getenv('DB_PASSWORD'),
+        #     "url": "dms2.citymall.dev",
+        #     "username": os.getenv('DB_USERNAME'),
+        #     "destination_type": "pgsql",
+        # },
         'ec2_2': {
             "db_name": "cmdb",
             "schema": "wms",
@@ -20,14 +20,14 @@ mapping = {
             "username": os.getenv('DB_USERNAME'),
             "destination_type": "pgsql",
         },
-        'ec2_3': {
-            "db_name": "cmdb",
-            "schema": "wms",
-            "password": os.getenv('DB_PASSWORD'),
-            "url": "dms3.citymall.dev",
-            "username": os.getenv('DB_USERNAME'),
-            "destination_type": "pgsql"            
-        }
+        # 'ec2_3': {
+        #     "db_name": "cmdb",
+        #     "schema": "wms",
+        #     "password": os.getenv('DB_PASSWORD'),
+        #     "url": "dms3.citymall.dev",
+        #     "username": os.getenv('DB_USERNAME'),
+        #     "destination_type": "pgsql"            
+        # }
     },
     "source": {
         "db_name": "wmsdb",
